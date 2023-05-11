509efd783a3ee4f21c69d214fa0efdfbff02bafe
BiblicallyAccurateMika <74475618+BiblicallyAccurateMika@users.noreply.github.com>
2023-05-11 13:33:54 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,22 +15,15 @@ mkdir -p ~/Downloads $dir_dotfiles $dir_keyrings $dir_setup
 # 
 
 # Downloading packages
-dpkg-query -s nala > /dev/null
+dpkg-query -s nala &> /dev/null
 install_nala=$?
 if [$install_nala -eq 1]
 	# wget https://gitlab.com/volian/volian-archive/uploads/b20bd8237a9b20f5a82f461ed0704ad4/volian-archive-keyring_0.1.0_all.deb --output-document=~/Downloads/volian-archive-keyring_0.1.0_all.deb
 	wget https://gitlab.com/volian/volian-archive/uploads/d6b3a118de5384a0be2462905f7e4301/volian-archive-nala_0.1.0_all.deb --output-document="$dir_setup/volian-archive-nala_0.1.0_all.deb"
 	dpkg install "$dir_setup/volian-archive-nala_0.1.0_all.deb"
 	apt update
-	apt install nala
+	apt install nala -y
 fi
-
-# nala install xorg lightdm awesome
-# 
-# # Linking Dotfiles
-# 
-# 
-
 
 # Remove SetupDir
 rm -r $dir_setup
