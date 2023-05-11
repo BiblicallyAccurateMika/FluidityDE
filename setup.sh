bbf93f35836752d2ad7cd39811f228cf8ad49a64
BAMikaHSS <michael.brewka@gmx.net>
2023-05-11 12:52:53 +0200

    Updated Setup

--- setup.sh
+++ setup.sh
@@ -7,11 +7,22 @@ echo "Test Selektion"
 echo "Creating Folders..."
 dir_dotfiles=~/Dotfiles
 dir_keyrings=~/.keyrings
-mkdir -p ~/Downloads $dir_dotfiles $dir_keyrings
+dir_setup=~/.setup
+mkdir -p ~/Downloads $dir_dotfiles $dir_keyrings $dir_setup
 
 # # Copying Dotfiles
 # echo "Copying Dotfiles into '~/Dotfiles'..."
 # 
+
+# Downloading packages
+dpkg-query -s nala > /dev/null
+install_nala=$?
+if [$install_nala -eq 1]
+	# wget https://gitlab.com/volian/volian-archive/uploads/b20bd8237a9b20f5a82f461ed0704ad4/volian-archive-keyring_0.1.0_all.deb --output-document=~/Downloads/volian-archive-keyring_0.1.0_all.deb
+	wget https://gitlab.com/volian/volian-archive/uploads/d6b3a118de5384a0be2462905f7e4301/volian-archive-nala_0.1.0_all.deb --output-document="$dir_setup/volian-archive-nala_0.1.0_all.deb"
+	dpkg install "$dir_setup/volian-archive-nala_0.1.0_all.deb"
+fi
+
 # # Downloading Keyrings
 # echo "Downloading Keyrings..."
 # wget https://gitlab.com/volian/volian-archive/uploads/b20bd8237a9b20f5a82f461ed0704ad4/volian-archive-keyring_0.1.0_all.deb --output-document=~/Downloads/volian-archive-keyring_0.1.0_all.deb
@@ -30,3 +41,7 @@ mkdir -p ~/Downloads $dir_dotfiles $dir_keyrings
 # # Linking Dotfiles
 # 
 # 
+
+
+# Remove SetupDir
+rm -r $dir_setup
