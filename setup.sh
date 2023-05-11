d06e9420bf15ee5779b529ae5c296cdb4e600520
BiblicallyAccurateMika <74475618+BiblicallyAccurateMika@users.noreply.github.com>
2023-05-11 13:37:19 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ if [ $install_nala -eq 1 ]
 	then
 	# wget https://gitlab.com/volian/volian-archive/uploads/b20bd8237a9b20f5a82f461ed0704ad4/volian-archive-keyring_0.1.0_all.deb --output-document=~/Downloads/volian-archive-keyring_0.1.0_all.deb
 	wget https://gitlab.com/volian/volian-archive/uploads/d6b3a118de5384a0be2462905f7e4301/volian-archive-nala_0.1.0_all.deb --output-document="$dir_setup/volian-archive-nala_0.1.0_all.deb"
-	dpkg install "$dir_setup/volian-archive-nala_0.1.0_all.deb"
+	apt install "$dir_setup/volian-archive-nala_0.1.0_all.deb"
 	apt update
 	apt install nala -y
 fi
